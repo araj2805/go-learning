b7419f25eb2503e3d14692362f7b5c189da3dec5
Ankit Raj <araj2805@gmail.com>
2024-03-08 17:36:54 +0530

    Revert "Added Multiple test in sinlge test case and make method to handle empty string"
    This reverts commit 92f17671d383497db292d6855e4025611f911494.
    
    Reverting back

--- my-project/hello_world.go
+++ my-project/hello_world.go
@@ -2,15 +2,10 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello "
-
 func Hello(name string) string {
-	if name == "" {
-		name = "World"
-	}
-	return helloPrefix + name
+	return "Hello "+name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("snkiy"))
 }
--- my-project/hello_world_test.go
+++ my-project/hello_world_test.go
@@ -3,36 +3,15 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Ankit")
-		want := "Hello Ankit "
-
-		// if got != want {
-		// 	t.Errorf("got %q want %q", got, want)
-		// }
-
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("saying empty paramter", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello World"
-
-		// if got != want {
-		// 	t.Errorf("got %q want %q", got, want)
-		// }
-
-		assertCorrectMessage(t, got, want)
-
-	})
-
-}
-
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-
+	got := Hello("Ankit")
+	want := "Hello Ankit"
+	
 	if got != want {
-		t.Errorf("got = %q, want = %q", got, want)
+	
+		t.Errorf("got %q want %q",got, want)
+	
 	}
+
+
+
 }

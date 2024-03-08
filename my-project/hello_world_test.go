92f17671d383497db292d6855e4025611f911494
Ankit Raj <araj2805@gmail.com>
2024-03-08 17:06:36 +0530

    Added Multiple test in sinlge test case and make method to handle empty string

--- my-project/hello_world_test.go
+++ my-project/hello_world_test.go
@@ -3,15 +3,36 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Ankit")
-	want := "Hello Ankit"
-	
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ankit")
+		want := "Hello Ankit "
+
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying empty paramter", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-	
-		t.Errorf("got %q want %q",got, want)
-	
+		t.Errorf("got = %q, want = %q", got, want)
 	}
-
-
-
 }

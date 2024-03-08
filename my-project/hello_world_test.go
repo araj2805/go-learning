8e52112e069835239dea21ac9337af4481cc12ac
Ankit Raj <araj2805@gmail.com>
2024-03-08 18:22:56 +0530

    Add Multiple sub test in single testcase and refactor testcase

--- my-project/hello_world_test.go
+++ my-project/hello_world_test.go
@@ -3,15 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Ankit")
-	want := "Hello Ankit"
-	
+
+	t.Run("Paramter Testing", func(t *testing.T) {
+		got := Hello("Ankit")
+		want := "Hello Ankit"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Empty Parameter", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-	
-		t.Errorf("got %q want %q",got, want)
-	
+		t.Errorf("got = %q, want = %q", got, want)
 	}
-
-
-
 }
